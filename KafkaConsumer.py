36890f9cc78388219556e53aa17e93a9a227157b
Sohail Hosseini <S.SADATHOSSEINI@GMAIL.COM>
2023-07-09 20:26:48 -0400

    Images added

--- KafkaConsumer.py
+++ KafkaConsumer.py
@@ -5,7 +5,10 @@ conf = {
     'bootstrap.servers': 'localhost:9092',
     'group.id': 'names_group',
     'auto.offset.reset': 'earliest',
-    'max.partition.fetch.bytes': 10 * 1024 * 1024  # 10 MB
+    'max.partition.fetch.bytes': 10 * 1024 * 1024,
+    'fetch.message.max.bytes': 10 * 1024 * 1024,  # 10 MB
+ 
+
 }
 
 consumer = Consumer(conf)
